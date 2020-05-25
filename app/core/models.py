a9029957ca551eeabc442d09195c603fcf7681fb
Piotr Kawa <piotr-kawa@outlook.com>
2020-05-25 23:19:55 +0200

    add recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -59,3 +59,18 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    time_minutes = models.IntegerField(null=False)
+    link = models.CharField(max_length=255, blank=True, null=False)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -63,3 +63,13 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Guacamole',
+            time_minutes=10
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
